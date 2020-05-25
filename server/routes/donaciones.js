673592eb2ce72bee6caabad995585010919c40f9
Eduardo Sandoval <edubizzle7@gmail.com>
2020-05-25 01:28:27 -0500

    Cupones ,donaciones y company styles
    Se agrego parte de donaciones completo y responsive . Se agrego una clase en la parte de copiar cupon y tel en el modal

--- server/routes/donaciones.js
+++ server/routes/donaciones.js
@@ -3,58 +3,59 @@ const Empresa = require("../models/empresa");
 const donaciones = require("../models/donaciones");
 const app = express();
 
-const { verificaToken } = require('../middlewares/autenticacion');
+const { verificaToken } = require("../middlewares/autenticacion");
 
 // LISTAR DONACIONES GENERALES
-app.get("/donaciones", function(req, res) {
-    donaciones.find({}).exec((err, donacion) => {
-        if (err) {
-            return res.status(400).json({
-                ok: false,
-                err,
-            });
-        }
+app.get("/donaciones", function (req, res) {
+  donaciones.find({}).exec((err, donacion) => {
+    if (err) {
+      return res.status(400).json({
+        ok: false,
+        err,
+      });
+    }
 
-        res.render("donaciones", {
-            ok: true,
-            donacion,
-        });
+    res.render("donaciones", {
+      ok: true,
+      donacion,
     });
+  });
 });
-
 
 //LISTAR DONACIONES DE UNA EMPRESA
-app.get("/donacionesEmpresa", function(req, res) {
-    cupon.find({}).exec((err, cupones) => {
-        if (err) {
-            return res.status(400).json({
-                ok: false,
-                err,
-            });
-        }
+app.get("/donacionesEmpresa", function (req, res) {
+  cupon.find({}).exec((err, cupones) => {
+    if (err) {
+      return res.status(400).json({
+        ok: false,
+        err,
+      });
+    }
 
-        res.json({
-            ok: true,
-            cupones,
-        });
+    res.json({
+      ok: true,
+      cupones,
     });
+  });
 });
 
-
-
 // POST DONACIONES
-app.post("/donaciones", verificaToken, function(req, res) {
-    console.log("POST /donaciones");
-    console.log(req.body);
-    let body = req.body;
-    body.empresa = req.empresa._id;
+app.post("/donaciones", verificaToken, function (req, res) {
+  console.log("POST /donaciones");
+  console.log(req.body);
+  let body = req.body;
+  body.empresa = req.empresa._id;
 
-    let DONAR = new donaciones(body);
+  let DONAR = new donaciones(body);
 
-    DONAR.save((err, donarDB) => {
-        if (err) res.status(500).send({ message: `Error: ${err}` });
-        res.render('register-success', { DONAR: donarDB, enlace: '/sistemaDonaciones', boton: 'Donaciones' });
+  DONAR.save((err, donarDB) => {
+    if (err) res.status(500).send({ message: `Error: ${err}` });
+    res.render("register-success", {
+      DONAR: donarDB,
+      enlace: "/sistemaDonaciones",
+      boton: "Donaciones",
     });
+  });
 });
 
 module.exports = app;
