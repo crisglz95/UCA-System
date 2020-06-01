0b4dd17b0a246a96eaf671c0a703760fd7266eff
crisglz95 <cristian.gonzalez.1806@gmail.com>
2020-06-01 14:58:17 -0500

    Commit Token Expirado

--- server/middlewares/autenticacion.js
+++ server/middlewares/autenticacion.js
@@ -7,12 +7,17 @@ let verificaToken = (req, res, next) => {
     // let token = req.get('token');
     let token = req.cookies.token;
 
+    if (!token) {
+        return res.render('token_finalizado', {});
+    }
+
     jwt.verify(token, process.env.SEED, (err, decoded) => {
         if (err) {
             return res.status(401).json({
                 ok: false,
                 err
             });
+            // return res.render('home', {});
         }
 
         req.empresa = decoded.empresa;
