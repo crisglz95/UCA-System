b53c9b0a291954fbd4707e3d23b182b1eedd1403
crisglz95 <cristian.gonzalez.1806@gmail.com>
2020-06-15 16:56:06 -0500

    Commit Logout Sistema

--- server/routes/login.js
+++ server/routes/login.js
@@ -7,80 +7,83 @@ const store = require("store");
 
 const app = express();
 
+app.get('/logout', (req, res) => {
+    res.clearCookie('token');
+    res.render('home', {});
+})
+
 app.post("/login", (req, res) => {
-  let body = req.body;
-  console.log(body);
+    let body = req.body;
+    console.log(body);
 
-  Empresa.findOne({ correo: body.correo }, (err, empresaDB) => {
-    if (err) {
-      return res.status(500).json({
-        ok: false,
-        err,
-      });
-    }
+    Empresa.findOne({ correo: body.correo }, (err, empresaDB) => {
+        if (err) {
+            return res.status(500).json({
+                ok: false,
+                err,
+            });
+        }
 
-    if (!empresaDB) {
-      res.render("login-error", {});
-    }
+        if (!empresaDB) {
+            res.render("login-error", {});
+        }
 
-    if (!bcrypt.compareSync(body.password, empresaDB.password)) {
-        res.render("login-error", {});
+        if (!bcrypt.compareSync(body.password, empresaDB.password)) {
+            res.render("login-error", {});
 
-    }
+        }
 
-    if (empresaDB.status === false) {
-      return res.render("validacion-pendiente", {
-        ok: false,
-        err: true,
-        message: "Empresa no validada",
-      });
-    }
+        if (empresaDB.status === false) {
+            return res.render("validacion-pendiente", {
+                ok: false,
+                err: true,
+                message: "Empresa no validada",
+            });
+        }
 
-    let token = jwt.sign(
-      {
-        empresa: empresaDB,
-      },
-      process.env.SEED,
-      { expiresIn: process.env.CADUCIDAD_TOKEN }
-    );
+        let token = jwt.sign({
+                empresa: empresaDB,
+            },
+            process.env.SEED, { expiresIn: process.env.CADUCIDAD_TOKEN }
+        );
 
-    console.log(empresaDB);
+        console.log(empresaDB);
 
-    res.cookie("token", token, { httpOnly: true });
+        res.cookie("token", token, { httpOnly: true });
 
-    // store.set('token', { token });
+        // store.set('token', { token });
 
-    // res.setHeader('token', token);
+        // res.setHeader('token', token);
 
-    if (empresaDB.administrador === true) {
-      return res.render("admin-sys", {
-        empresaDB,
-        ok: true,
-        empresa: empresaDB,
-      });
-    }
+        if (empresaDB.administrador === true) {
+            return res.render("admin-sys", {
+                empresaDB,
+                ok: true,
+                empresa: empresaDB,
+            });
+        }
 
-    res.render("home-sys", {
-      empresaDB,
-      ok: true,
-      empresa: empresaDB,
+        res.render("home-sys", {
+            empresaDB,
+            ok: true,
+            empresa: empresaDB,
+        });
+
+        // res.json({
+        //     ok: true,
+        //     empresa: empresaDB,
+        // });
+
+        // let token = jwt.sign({
+        //     usuario: usuarioDB
+        // }, process.env.SEED, { expiresIn: process.env.CADUCIDAD_TOKEN });
+
+        // res.json({
+        //     ok: true,
+        //     usuario: usuarioDB,
+        //     token
+        // });
     });
-
-    // res.json({
-    //     ok: true,
-    //     empresa: empresaDB,
-    // });
-
-    // let token = jwt.sign({
-    //     usuario: usuarioDB
-    // }, process.env.SEED, { expiresIn: process.env.CADUCIDAD_TOKEN });
-
-    // res.json({
-    //     ok: true,
-    //     usuario: usuarioDB,
-    //     token
-    // });
-  });
 });
 
 module.exports = app;
